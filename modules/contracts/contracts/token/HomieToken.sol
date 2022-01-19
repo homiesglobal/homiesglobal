8cbafb523a96fb50205b3a728a587ab2a0397d02
Bezaeel <talabi.rotimi@hotmail.com>
2022-01-19 23:23:16 +0100

    lint error fix

--- modules/contracts/contracts/token/HomieToken.sol
+++ modules/contracts/contracts/token/HomieToken.sol
@@ -4,11 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HomieToken is ERC20 {
-
     constructor() ERC20("Homie", "HOMIE") {
-        _mint(msg.sender, 500000 * (10 ** uint8(decimals())));
+        _mint(msg.sender, 500000 * (10**uint8(decimals())));
     }
-    
+
     function decimals() public pure override returns (uint8) {
         return 2;
     }
