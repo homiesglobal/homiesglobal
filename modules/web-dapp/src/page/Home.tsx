79d0f2d8ee83aa5771efb48a97f7f32246bd27c9
Perfect Makanju <damiperfect@gmail.com>
2022-02-06 18:06:35 +0000

    wip: Home Styling

--- modules/web-dapp/src/page/Home.tsx
+++ modules/web-dapp/src/page/Home.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { Typography, Row, Col } from "antd";
+import { Logo } from "../components/Logo";
+import { WalletLoginButton } from "../components/WalletLoginButton";
+
+const { Title, Text } = Typography;
+
+const styles = {
+  content: {
+    textAlign: "center",
+  },
+  title: {},
+};
 
 export const Home: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Row justify="center">
+        <Col style={{ textAlign: "center" }}>
+          <Logo />
+          <Title style={styles.title} level={2}>
+            Introduction to HOMIE
+          </Title>
+          <div>
+            <Text>
+              With the launch of HOMIE and the DAO, the community will be
+              empowered to govern the Homie Token
+            </Text>
+          </div>
+          <WalletLoginButton />
+        </Col>
+      </Row>
+    </div>
+  );
 };
