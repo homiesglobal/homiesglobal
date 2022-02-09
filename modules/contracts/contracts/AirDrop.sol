22ed57ba25c6e3dcd8fb8cfc53f092bafba68b51
Bezaeel <talabi.rotimi@hotmail.com>
2022-02-09 18:01:26 +0100

    contract: renamed filefrom poc to actual

--- modules/contracts/contracts/AirDrop.sol
+++ modules/contracts/contracts/AirDrop.sol
@@ -1,25 +1,45 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract AirDrop{
-    address userAddr;
-    mapping (address=>bool) listOfAddr;
-    mapping(address => bool) claims;
-    constructor(address _data){
-      userAddr = _data;   
-   }
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-   function claimToken(address _user )public{
-    
-       if(listOfAddr[_user] && !claims[_user]){
+contract AirDrop_ {
+    mapping (address=>bool) allowed;
+    mapping (address=>bool) claims;
+    address admin;
+    uint256 amountToBeClaimed;
 
-           emit SuccessEvent(msg.sender, "success", SuccessEventTYPE.CLAIMSUCCESS);
-       }
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
 
-   }
+    event tokenClaimed(address indexed recipient, string message);
+    event recipientAdded(address recipient, string message);
 
-   enum SuccessEventTYPE{CLAIMSUCCESS, ADDSUCCESS, SWAPSUCCESS}
+    IERC20 homieToken;
 
-   event SuccessEvent(address indexed _from, string message, SuccessEventTYPE);
+    constructor(address _tokenAddress) {
+        homieToken = IERC20(_tokenAddress);
+    }
+
+    function AddRecipient(address recipient) public onlyAdmin {
+        allowed[recipient] = true;
+        emit recipientAdded(recipient, "recipient added");
+    }
+
+    function setAmountToBeClaimed(uint256 amount) public onlyAdmin {
+        amountToBeClaimed = amount;
+    }
+
+    function ClaimToken(address recipient) public payable {
+        require(!allowed[recipient], "only allowed address");
+        require(claims[recipient], "tokens have been claimed");
+        // this prevents recipient from claiming token more than once
+        allowed[recipient] = false;
+        claims[recipient] = true;
+        homieToken.transferFrom(msg.sender, recipient, amountToBeClaimed);
+        emit tokenClaimed(recipient, "claimed token");
+    }
 
 }
