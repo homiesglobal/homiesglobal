37bbc3c621a6212790c100d4def6012e08e37b7b
Bezaeel <talabi.rotimi@hotmail.com>
2022-02-21 23:19:51 +0100

    fix airdrop bug on constructor

--- modules/contracts/contracts/AirDrop.sol
+++ modules/contracts/contracts/AirDrop.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract AirDrop {
-    mapping (address=>bool) allowed;
+    mapping (address=>bool) public allowed;
     mapping (address=>bool) claims;
     address public admin;
-    uint256 amountToBeClaimed;
+    uint256 public amountToBeClaimed;
 
-    modifier onlyAdmin() {
+    modifier onlyOwner() {
+        console.log("msg.sender from modifier:::::", msg.sender);
         require(msg.sender == admin, "only admin can call function");
         _;
     }
@@ -21,23 +23,30 @@ contract AirDrop {
 
     constructor(address _tokenAddress) {
         homieToken = IERC20(_tokenAddress);
-        admin == msg.sender;
+        admin = msg.sender;
     }
 
-    function AddRecipient(address recipient) public onlyAdmin {
+    function AddRecipient(address recipient) public onlyOwner {
         allowed[recipient] = true;
         emit recipientAdded(recipient, "recipient added");
     }
 
-    function setAmountToBeClaimed(uint256 amount) external onlyAdmin {
+    function getAmountToBeClaimed() external view returns (uint256) {
+        console.log(msg.sender);
+        console.log("admin::::", admin);
+        require(msg.sender == admin, "msg.sender is not equal admin");
+        return amountToBeClaimed;
+    }
+
+    function setAmountToBeClaimed(uint256 amount) public onlyOwner {
         amountToBeClaimed = amount;
     }
 
     function ClaimToken(address recipient) public payable {
-        require(!allowed[recipient], "only allowed address");
-        require(claims[recipient], "tokens have been claimed");
+        require(allowed[recipient], "only allowed address");
+        require(!claims[recipient], "tokens have been claimed");
         // this prevents recipient from claiming token more than once
-        allowed[recipient] = false;
+        allowed[recipient] = true;
         claims[recipient] = true;
         homieToken.transferFrom(msg.sender, recipient, amountToBeClaimed);
         emit tokenClaimed(recipient, "claimed token");
