2727ba174b9e019e7dbac2369b4ed951752ff01c
Bezaeel <talabi.rotimi@hotmail.com>
2022-02-21 23:20:05 +0100

    airdrop unit test

--- modules/contracts/test/Airdrop.test.ts
+++ modules/contracts/test/Airdrop.test.ts
@@ -1,52 +1,78 @@
-// // import { expect } from "chai";
-// import AirdropArtifact from "../artifacts/contracts/AirDrop.sol/AirDrop.json";
-// import { deployContract } from "ethereum-waffle";
-// import { ethers } from "hardhat";
+// import { expect } from "chai";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
 
-// describe("Airdrop contract", function () {
-//   it.only("Should able to claim token ", async function () {
-//     const accounts = await ethers.getSigners();
-//     for (const account of accounts) {
-//       console.log(account.address);
-//     }
+describe.only("Airdrop contract", async function () {
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addr3: SignerWithAddress;
 
-//     const HomieToken = await ethers.getContractFactory("HomieToken");
-//     const homieToken = await HomieToken.deploy();
+  before(async () => {
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
+  });
 
-//     // const Airdrop = await ethers.getContractFactory("AirDrop");
-//     const airdrop = await deployContract(accounts[2], AirdropArtifact, [
-//       homieToken.address,
-//     ]);
+  // only admin should add user
+  it("Should able to claim token", async function () {
+    const HomieToken = await ethers.getContractFactory("HomieToken");
+    const homieToken = await HomieToken.deploy();
+    await homieToken.deployed();
+    const Airdrop = await ethers.getContractFactory("AirDrop");
+    const airdrop = await Airdrop.deploy(homieToken.address);
+    await airdrop.deployed();
 
-//     const accounts = await ethers.getSigners();
-//     for (const account of accounts) {
-//       console.log(account.address);
-//     }
-//     const airdrop = await Airdrop.deploy();
-//     await airdrop.deployed();
-//     console.log("airdrop deployed at:::::", airdrop.address);
+    await airdrop.AddRecipient(addr1.address);
+    await airdrop.AddRecipient(addr2.address);
+    console.log("Ask Talabi...");
+    const allowed = airdrop.allowed;
+    const isExist = await allowed(addr1.address);
+    const notExist = await allowed(addr3.address);
+    // eslint-disable-next-line no-unused-expressions
+    expect(isExist).to.be.true;
+    // eslint-disable-next-line no-unused-expressions
+    expect(notExist).to.be.false;
+  });
 
-//     await homieToken.deployed();
+  // only admin should set amount
+  it("Should able to set amount to be claimed", async function () {
+    const HomieToken = await ethers.getContractFactory("HomieToken");
+    const homieToken = await HomieToken.deploy();
+    await homieToken.deployed();
+    const Airdrop = await ethers.getContractFactory("AirDrop");
+    const airdrop = await Airdrop.deploy(homieToken.address);
+    await airdrop.deployed();
+    let amountToBeClaimed = await airdrop.getAmountToBeClaimed();
+    console.log("getAmountToBeClaimed::::::", amountToBeClaimed);
 
-//     console.log("owner address::::", accounts[0].address);
-//     console.log("airdrop deployed at:::::", airdrop.address);
-//     console.log("homie deployed at:::::", homieToken.address);
+    await airdrop.connect(owner).setAmountToBeClaimed(5000000);
+    amountToBeClaimed = await airdrop.getAmountToBeClaimed();
 
-//     await airdrop.connect(accounts[1]).setAmountToBeClaimed(5000000);
+    console.log("getAmountToBeClaimed2::::::", amountToBeClaimed);
+    expect(amountToBeClaimed).to.equal(5000000);
+  });
 
-//     // send money from token to airdrop address
-//     // await homieToken.transfer(airdrop.address, 25000000);
-//     // const claimBalance = await homieToken.balanceOf(airdrop.address);
-//     // console.log("claim balance:::::::::", claimBalance);
-//     // // check balance of recipient before
-//     // const recipientBalance = await homieToken.balanceOf(addr.address);
-//     // console.log("recipient balance before::::::", recipientBalance);
+  // should not claim token more than once
+  it.only("should not claim token more than once", async function () {
+    const HomieToken = await ethers.getContractFactory("HomieToken");
+    const homieToken = await HomieToken.deploy();
+    await homieToken.deployed();
+    const Airdrop = await ethers.getContractFactory("AirDrop");
+    const airdrop = await Airdrop.deploy(homieToken.address);
+    await airdrop.deployed();
 
-//     // await airdrop.AddRecipient(addr[3].address);
-//     // await airdrop.AddRecipient(addr2);
-//     // await airdrop.AddRecipient(addr3);
-//     // await airdrop.ClaimToken(addr[3].address);
+    await airdrop.AddRecipient(addr1.address);
+    await airdrop.AddRecipient(addr2.address);
+    const oldBalance = await homieToken.balanceOf(addr1.address);
+    console.log("oldBalance::::", oldBalance);
+    await airdrop.connect(addr1).ClaimToken(addr1.address);
+    // const amount = await airdrop.amountToBeClaimed;
+    // check user balance
+    const newBalance = await homieToken.balanceOf(addr1.address);
+    console.log("newBalance::::::", newBalance);
 
-//     // expect(await homieToken.balanceOf(addr[3].address)).to.equal(25000000);
-//   });
-// });
+    await expect(
+      airdrop.connect(addr1).ClaimToken(addr1.address)
+    ).to.be.revertedWith("tokens have been claimed");
+  });
+});
