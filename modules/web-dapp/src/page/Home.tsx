e6cc1712901da6fbaa37fc18fef647b8c365d816
Perfect Makanju <damiperfect@gmail.com>
2022-02-26 11:56:42 +0000

    revert: stop using VerticalCenter component

--- modules/web-dapp/src/page/Home.tsx
+++ modules/web-dapp/src/page/Home.tsx
@@ -6,27 +6,24 @@ import styles from "./Home.module.css";
 import { Logo } from "../components/Icons/Logo";
 import { AppRoute } from "../config/routes";
 import { Button } from "../components/Button/Button";
-import { VerticallyCenter } from "../components/Layout/VerticallyCenter";
 
 export const Home: React.FC = () => {
   return (
     <Row justify="center">
       <Col sm={{ span: 24 }} md={{ span: 12 }} className={styles.content}>
-        <VerticallyCenter>
-          <Logo />
-          <h1 className={styles.title}>
-            Introduction to HOMIE <ArrowRightOutlined />
-          </h1>
-          <p className={styles.subtitle}>
-            With the launch of HOMIE and the DAO, the community will be
-            empowered to govern the Homie Token
-          </p>
-          <Link to={AppRoute.ConnectWallet}>
-            <Button className={styles.connectButton} type="primary">
-              Connect Wallet
-            </Button>
-          </Link>
-        </VerticallyCenter>
+        <Logo />
+        <h1 className={styles.title}>
+          Introduction to HOMIE <ArrowRightOutlined />
+        </h1>
+        <p className={styles.subtitle}>
+          With the launch of HOMIE and the DAO, the community will be empowered
+          to govern the Homie Token
+        </p>
+        <Link to={AppRoute.ConnectWallet}>
+          <Button className={styles.connectButton} type="primary">
+            Connect Wallet
+          </Button>
+        </Link>
       </Col>
     </Row>
   );
